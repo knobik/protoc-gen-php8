15ece6e93cba88a8a7c83a4c0bd2af5aa00b4a37
knobik <knobiks@gmail.com>
2023-12-11 11:48:57 +0100

    small refactor of options

--- protoabs/options.go
+++ protoabs/options.go
@@ -20,18 +20,20 @@ func ParseOptions(parameter *string) (*Options, error) {
 		args := strings.Split(*parameter, ",")
 		for _, a := range args {
 			parts := strings.Split(a, "=")
+			opt := parts[0]
+			val := parts[1]
 
-			ok, err := reflections.HasField(o, parts[0])
+			ok, err := reflections.HasField(o, opt)
 			if err != nil {
 				return nil, err
 			}
 			if ok {
-				err = reflections.SetField(o, parts[0], parts[1])
+				err = reflections.SetField(o, opt, val)
 				if err != nil {
 					return nil, err
 				}
 			} else {
-				return nil, errors.New(fmt.Sprintf("option %s does not exist", parts[0]))
+				return nil, errors.New(fmt.Sprintf("option %s does not exist", opt))
 			}
 		}
 	}
